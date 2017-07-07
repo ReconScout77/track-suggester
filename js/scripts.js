fa129567b732ca860356793df9e2ee7425d3b0fa
Robert Murray <alexmurray93@hotmail.com>
2017-07-07 14:54:29 -0700

    Add else if statements to give a more personalized message if the user enters in a name.

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,19 @@ $(document).ready(function() {
     var abcInput = $("input:radio[name=abc]:checked").val();
     var numInput = parseInt($("input#num").val());
 
-    if (nameInput, programmerTypeInput && interestInput && workplaceInput && abcInput && numInput) {
+    if (nameInput && programmerTypeInput && interestInput && workplaceInput && abcInput && numInput) {
+      if(interestInput === "mobile") {
+        $("#result").text(nameInput + ", since you are interested in mobile development you should try the Java track!");
+      } else if (programmerTypeInput === "front" && interestInput === "any" && workplaceInput != "govern") {
+        $("#result").text(nameInput + ", you seem like you want to make things look amazing, you should take the CSS track!");
+      } else if (workplaceInput === "small" || numInput < 5) {
+        $("#result").text(nameInput + ", the Ruby track looks just right for you!");
+      } else if (programmerTypeInput === "back" && abcInput != "c") {
+        $("#result").text(nameInput + ", you seem like the sort who would do well in the PHP track!");
+      } else {
+        $("#result").text("Well then, " + nameInput + ", you should try the C# track!");
+      }
+    } else if (nameInput, programmerTypeInput && interestInput && workplaceInput && abcInput && numInput) {
       if(interestInput === "mobile") {
         $("#result").text("Java!");
       } else if (programmerTypeInput === "front" && interestInput === "any" && workplaceInput != "govern") {
