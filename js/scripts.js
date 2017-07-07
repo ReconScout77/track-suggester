d28c56fb1b1ac9a9ae3545fe243c7be9d95ea4b4
Robert Murray <alexmurray93@hotmail.com>
2017-07-07 13:06:35 -0700

    Add another set of radio buttons, a number input, and modified if statement requirements.

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,17 @@ $(document).ready(function() {
     var programmerTypeInput = $("input:radio[name=programmerType]:checked").val();
     var interestInput = $("#interest").val();
     var workplaceInput = $("#workplace").val();
+    var abcInput = $("input:radio[name=abc]:checked").val();
+    var numInput = parseInt($("input#num").val());
 
     if (nameInput, programmerTypeInput && interestInput && workplaceInput) {
       if(interestInput === "mobile") {
         $("#result").text("Java!");
       } else if (programmerTypeInput === "front" && interestInput === "any" && workplaceInput != "govern") {
         $("#result").text("CSS!");
-      } else if (workplaceInput === "small") {
+      } else if (workplaceInput === "small" || numInput < 5) {
         $("#result").text("Ruby!");
-      } else if (programmerTypeInput === "back") {
+      } else if (programmerTypeInput === "back" && abcInput != "c") {
         $("#result").text("PHP!");
       } else {
         $("#result").text("C#!");
