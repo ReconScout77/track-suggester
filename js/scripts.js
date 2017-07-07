617b9635be95be77677bb2265674f715eca83c74
Robert Murray <alexmurray93@hotmail.com>
2017-07-07 11:12:13 -0700

    Add another select box and change input requirements for showing certain results.

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,19 @@ $(document).ready(function() {
     var nameInput = $("input#name").val();
     var programmerTypeInput = $("input:radio[name=programmerType]:checked").val();
     var interestInput = $("#interest").val();
+    var workplaceInput = $("#workplace").val();
 
-    if (nameInput, programmerTypeInput && interestInput) {
+    if (nameInput, programmerTypeInput && interestInput && workplaceInput) {
       if(interestInput === "mobile") {
         $("#java").show();
-      } else if (programmerTypeInput === "front" && interestInput === "any") {
+      } else if (programmerTypeInput === "front" && interestInput === "any" && workplaceInput != "govern") {
         $("#css").show();
-      } else if (interestInput === "web") {
-        $("#c").show();
+      } else if (workplaceInput === "small") {
+        $("#ruby").show();
       } else if (programmerTypeInput === "back") {
         $("#php").show();
       } else {
-        $("#ruby").show();
+        $("#c").show();
       }
     }
   });
