b5fc26eafbd67207854c3c699a486679c5b67cc7
Robert Murray <alexmurray93@hotmail.com>
2017-07-07 10:34:29 -0700

    Add select box, hidden results for c, java, php, ruby, and css, add else if statements to allow for each result to show up depending on the answers.

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,20 @@ $(document).ready(function() {
 
     var nameInput = $("input#name").val();
     var programmerTypeInput = $("input:radio[name=programmerType]:checked").val();
+    var interestInput = $("#interest").val();
 
-    if (nameInput, programmerTypeInput) {
-      $("#result").show();
+    if (nameInput, programmerTypeInput && interestInput) {
+      if(interestInput === "mobile") {
+        $("#java").show();
+      } else if (programmerTypeInput === "front" && interestInput === "any") {
+        $("#css").show();
+      } else if (interestInput === "web") {
+        $("#c").show();
+      } else if (programmerTypeInput === "back") {
+        $("#php").show();
+      } else {
+        $("#ruby").show();
+      }
     }
-  })
-})
+  });
+});
